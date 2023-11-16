f2c59e10aeed86e64e9707d5dadff5721acdb62f
Sebinkarahisar MYO <151005412+Recepexe@users.noreply.github.com>
2023-11-16 10:17:04 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           DialogResult sonuc;
+            sonuc = MessageBox.Show("Evet Butonuna Basarsanız Kayıt Silinecektir.\nKayıt Silinme İşlemini Onaylıyor musunuz?"
+                , "DİKKAT"
+                , MessageBoxButtons.YesNo
+                , MessageBoxIcon.Question
+                , MessageBoxDefaultButton.Button1);
+
+            if (sonuc == DialogResult.Yes)
+            {
+                MessageBox.Show("Kayıt Silindi.");
+            }
+            else
+            {
+                MessageBox.Show("kayıt silme işlemi iptal edildi.");
+            }
+        }
+        }
+    }
+
